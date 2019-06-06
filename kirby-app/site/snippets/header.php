e9f44f597f7bdf02efe9ce8c27f8b0f11c58c692
Julien Belin <julienbelin002@gmail.com>
2019-06-06 13:40:56 +0200

    Adding analytics tracking

--- kirby-app/site/snippets/header.php
+++ kirby-app/site/snippets/header.php
@@ -2,18 +2,28 @@
 <html lang="en">
 <head>
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141591076-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-141591076-1');
+  </script>
+
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
 
+  <!-- jQuery library -->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-
-    <script type="text/javascript" src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
-
-  <!-- AddEvent script -->
+  <!-- Isotope library -->
+  <script type="text/javascript" src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+  <!-- AddEvent library -->
   <script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
-
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
@@ -23,4 +33,3 @@
 <body>
 
   <div class="page">
-  
